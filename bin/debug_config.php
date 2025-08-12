1311931a2ecdb86fceed449f222dbfb672b680a4
Hawk1nson <andrew.hawkinson@my.metrostate.edu>
2025-08-12 13:35:50 -0500

    Create debug_config.php
    created debug_config.php to make debugging faster.  just include or uncomment debug_config.php to view errors on page.

---
+++ bin/debug_config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Debug configuration file
+ * Enable full error reporting for development
+ * 
+ * USAGE:
+ *      include 'debug_config.php';
+ * 
+ * IMPORTANT:
+ * - Do NOT include this in production unless actively debugging.
+ * - Comment out the 'include "bin/debug_config.php';"
+ */
+
+// Display all errors
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+?>
