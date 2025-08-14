5dc8e1a5846306fa488903b448d25d853be8ff5e
Hawk1nson <andrew.hawkinson@my.metrostate.edu>
2025-08-14 16:19:58 -0500

    Created reusable under_construction.php template and update modify_celebration.php to include it

---
+++ modify_celebration.php
@@ -0,0 +1,13 @@
+<?php
+// require_once 'bin/debug_config.php'; //uncomment if debugging is needed
+
+session_start();
+
+
+$page_title = 'Project ABCD > Modify Celebrations - Under Construction';
+include('header.php');
+include('under_construction.php');
+?>
+
+
+
