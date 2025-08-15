e38e00052f15395ab68c585b8c4d77aa54bc2c79
Hawk1nson <andrew.hawkinson@my.metrostate.edu>
2025-08-14 20:39:58 -0500

    Enabled MySQLi error reporting in debug_config.php

--- bin/debug_config.php
+++ bin/debug_config.php
@@ -16,4 +16,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+// Enable MySQLi error reporting
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
 ?>
